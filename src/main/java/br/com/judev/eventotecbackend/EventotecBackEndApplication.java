c06a2e99ba19675034e2284bfdea79be4a1c8b1a
Junior-Stranner <JuniorStranner@gmail.com>
2024-06-26 16:28:22 -0300

    feat : creating domains for project now gonna start with repositorys

---
+++ src/main/java/br/com/judev/eventotecbackend/EventotecBackEndApplication.java
@@ -0,0 +1,13 @@
+package br.com.judev.eventotecbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventotecBackEndApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EventotecBackEndApplication.class, args);
+    }
+
+}
---
+++ src/test/java/br/com/judev/eventotecbackend/EventotecBackEndApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.judev.eventotecbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventotecBackEndApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
